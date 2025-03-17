91fb7e8513b27569ee2e8a134c3bc9bdd2b35476
Flippyfun02 <amiebinan@gmail.com>
2025-03-16 22:25:26 -0400

    07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  celsius = (farenheit - 32) * (5/9);
+  if (celsius % 1 == 0) {
+    return celsius;
+  }
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  farenheit = (celsius * (9/5)) + 32;
+  if (farenheit % 1 == 0) {
+    return farenheit;
+  }
+  return parseFloat(farenheit.toFixed(1));
 };
 
 // Do not edit below this line
