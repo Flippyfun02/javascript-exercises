7e10e1432f2fb5d4379c3fedea354d8ef7b94d81
Flippyfun02 <amiebinan@gmail.com>
2025-03-16 21:51:22 -0400

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ... items) {
+    i = 0;
+    while (i < arr.length) {
+        for (var item of items) {
+            if (arr[i] === item) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+        i++;
+    }
+    return arr;
+
+    /* for (i = 0; i < arrLength; i++) {
+        for (var item of items) {
+            if (arr[i] === item) {
+                arr.splice(i, 1);
+            }
+        }
+    }
+    return arr; */
 };
 
 // Do not edit below this line
