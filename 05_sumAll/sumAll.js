ac69367ef6805b12fe3fc980adceae6c5959f43a
Flippyfun02 <amiebinan@gmail.com>
2025-03-16 22:04:53 -0400

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, finish) {
+    if (typeof start != "number" || typeof finish != "number" || start < 0 || 
+        finish < 0 || start % 1 != 0 || finish % 1 != 0) {
+        return "ERROR"
+    }
+    else if (start > finish) {
+        let temp = start;
+        start = finish;
+        finish = temp;
+    }
+    let sum = 0;
+    for (i = start; i <= finish; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
