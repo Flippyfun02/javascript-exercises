a09cabf4f3b0f49f339321b53227e530424c58f6
Flippyfun02 <amiebinan@gmail.com>
2025-03-16 21:02:58 -0400

    02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    rString = "";
+    for (i = 0; i < num; i++) {
+        rString += str;
+    }
+    return rString;
 };
 
 // Do not edit below this line
