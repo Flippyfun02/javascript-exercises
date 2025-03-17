dcc09e09c60ddda9f95dff780975f284fca94b02
Flippyfun02 <amiebinan@gmail.com>
2025-03-16 21:16:47 -0400

    03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    rString = "";
+    for (i = str.length - 1; i >= 0; i--) {
+        rString += str[i];
+    }
+    return rString;
 };
 
 // Do not edit below this line
